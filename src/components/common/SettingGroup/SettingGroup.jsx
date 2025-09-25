4415d63ef9acca230460c20169cd749f68bd7c5e
cheng <chengshope@gmail.com>
2025-09-24 20:50:20 -0600

    Create SettingGroup component

---
+++ src/components/common/SettingGroup/SettingGroup.jsx
@@ -0,0 +1,41 @@
+import { Card, Flex, Typography } from 'antd';
+
+const { Title, Text } = Typography;
+
+const SettingGroup = ({
+  title,
+  description,
+  children,
+  style = {},
+  titleLevel = 5,
+  gap = 16,
+  ...props
+}) => {
+  return (
+    <Card.Grid
+      style={{
+        width: '100%',
+        borderRadius: 6,
+        ...style,
+      }}
+      hoverable={false}
+      {...props}
+    >
+      <Flex vertical gap={gap}>
+        <Flex align="center" gap={12}>
+          <Title level={titleLevel} style={{ margin: 0 }}>
+            {title}
+          </Title>
+          {description && (
+            <Text style={{ paddingTop: 3 }} type="secondary">
+              {description}
+            </Text>
+          )}
+        </Flex>
+        <Flex vertical>{children}</Flex>
+      </Flex>
+    </Card.Grid>
+  );
+};
+
+export default SettingGroup;
