9993a31eb13eb702bbebc0a5ed00c80a23b8d095
cheng <chengshope@gmail.com>
2025-11-12 07:43:13 -0700

    Foramt code

--- src/features/existingCustomers/existingCustomersSelector.js
+++ src/features/existingCustomers/existingCustomersSelector.js
@@ -68,13 +68,11 @@ export const getFormattedSummaryData = createSelector(
     }
 
     const totalTenants = sum_tenants.toLocaleString();
-    const averageRateIncrease =
-      fc_length > 0 ? (sum_avr_rate_inc / fc_length).toFixed(2) : '0.00';
+    const averageRateIncrease = fc_length > 0 ? (sum_avr_rate_inc / fc_length).toFixed(2) : '0.00';
     const estimatedRevenueIncrease = (
       sum_tenants > 0 ? sum_rev_inc / sum_tenants : 0
     ).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-    const averageMoveOutProbability =
-      fc_length > 0 ? (sum_avr_mop / fc_length).toFixed(1) : '0.0';
+    const averageMoveOutProbability = fc_length > 0 ? (sum_avr_mop / fc_length).toFixed(1) : '0.0';
 
     return {
       totalTenants,
